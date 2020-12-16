dd6bc008c2446921b79bd022094135a315ecf010
sgeese1 <geesey1@live.com>
2020-12-15 20:51:30 -0500

    Day 2 and day 15 solutions

---
+++ day2.py
@@ -0,0 +1,22 @@
+
+import fileinput
+import re
+
+# 1-3 a: abcde
+p = re.compile(r"^(\d+)-(\d+) (.): (.*)$")
+
+s1 = 0
+s2 = 0
+
+for line in fileinput.input():
+    (low, high, letter, password) = p.match(line.strip()).groups()
+    low, high = int(low), int(high)
+
+    if low <= password.count(letter) <= high:
+        s1 += 1
+
+    if (password[low - 1] == letter) ^ (password[high - 1] == letter):
+        s2 += 1
+
+print(s1)
+print(s2)
